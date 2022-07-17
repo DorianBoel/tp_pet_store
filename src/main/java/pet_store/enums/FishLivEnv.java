64b1a89a4aa8a68d1289edcbb96dd113b528e373
DorianBoel <dorian.boel@free.fr>
2022-07-17 19:14:35 +0200

    add enums

---
+++ src/main/java/pet_store/enums/FishLivEnv.java
@@ -0,0 +1,8 @@
+package pet_store.enums;
+
+public enum FishLivEnv {
+
+	FRESH_WATER,
+	SEA_WATER;
+	
+}
---
+++ src/main/java/pet_store/enums/ProdType.java
@@ -0,0 +1,9 @@
+package pet_store.enums;
+
+public enum ProdType {
+
+	FOOD,
+	ACCESSORY,
+	CLEANING;
+	
+}
